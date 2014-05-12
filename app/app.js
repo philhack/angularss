6f3a99e0cea5d279a92a2ac8ddfdd0fe82aa5095
Phil Hack <phil@philhack.com>
2014-05-12 08:03:43 +0100

    added http interceptor

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 var huApp = angular.module('huApp', ['ngRoute', 'ngCookies', 'ui.router']);
 
-huApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
-                function($stateProvider, $urlRouterProvider, $locationProvider) {
+huApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider',
+                function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider.otherwise("/");
 
     $stateProvider
@@ -17,4 +17,14 @@ huApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
         })
     $locationProvider.html5Mode(true);
 
+    $httpProvider.interceptors.push(function($q, $location) {
+        return {
+            'responseError': function(response) {
+                if(response.status === 401 || response.status === 403) {
+                    $location.path('/login');
+                }
+                return $q.reject(response);
+            }
+        };
+    });
 }]);
